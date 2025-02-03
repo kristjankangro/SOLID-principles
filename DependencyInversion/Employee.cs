5c7f29d8528b6f4a114eecb5c4799a50a64a1a0a
kristjan kangro <kristjan.kangro@visma.com>
2025-02-03 23:19:01 +0200

    DI

--- DependencyInversion/Employee.cs
+++ DependencyInversion/Employee.cs
@@ -6,11 +6,11 @@ public class Employee
     public string EmployeeType { get; set; }
     public double TotalHoursWorked { get; set; }
 
-    public ILogger logger;
+    public ILogger Logger;
 
-    public Employee(ILogger InputLogger)
+    public Employee(ILogger inputLogger)
     {
-        logger = InputLogger;
+        Logger = inputLogger;
     }
 
     public void Save(Employee emp)
@@ -24,19 +24,24 @@ public class Employee
         catch (Exception ex)
         {
             
-            var isFileLog = true;
-            var isDBLog = false;
-            if (isFileLog)
-            {
-                ILogger fileLogger = new FileLogger();
-                fileLogger.LogError(ex.Message);
-            }
+            // #1 old
+            // var isFileLog = true;
+            // var isDBLog = false;
+            // if (isFileLog)
+            // {
+            //     ILogger fileLogger = new FileLogger();
+            //     fileLogger.LogError(ex.Message);
+            // }
+            //
+            // if (isDBLog)
+            // {
+            //     ILogger logger = new DatabaseLogger();
+            //     logger.LogError(ex.Message);
+            // }
             
-            if (isDBLog)
-            {
-                ILogger logger = new DatabaseLogger();
-                logger.LogError(ex.Message);
-            }
+            Logger.LogError(ex.Message);
+            
+            
         }
 
     }
--- DependencyInversion/Program.cs
+++ DependencyInversion/Program.cs
@@ -1,18 +1,24 @@
-﻿namespace DependencyInversion
+﻿namespace DependencyInversion;
+using Unity;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        IUnityContainer container = new UnityContainer();
+        container.RegisterType<ILogger, FileLogger>();
+        container.RegisterType<ILogger, DatabaseLogger>();
+        Employee employee = container.Resolve<Employee>();
+        employee.Save(employee);
 
-            Employee empFTE = new Employee(new FileLogger()) { EmployeeType = EmpType.FullTime.ToString(), TotalHoursWorked = 10 };
-            Employee empPTE = new Employee(new FileLogger()) { EmployeeType = EmpType.PartTime.ToString(), TotalHoursWorked = 10 };
-            Employee empContractor = new Employee(new FileLogger()) { EmployeeType = EmpType.Contractor.ToString(), TotalHoursWorked = 10 };
-
-            empFTE.Save(empFTE);
-       
-            Console.Read();
-        } 
-    }
+        // old after unity DI
+        // Employee empFTE = new Employee(new FileLogger()) { EmployeeType = EmpType.FullTime.ToString(), TotalHoursWorked = 10 };
+        // Employee empPTE = new Employee(new FileLogger()) { EmployeeType = EmpType.PartTime.ToString(), TotalHoursWorked = 10 };
+        // Employee empContractor = new Employee(new FileLogger()) { EmployeeType = EmpType.Contractor.ToString(), TotalHoursWorked = 10 };
+        //
+        // empFTE.Save(empFTE);
+        //
+        // Console.Read();
+    } 
 }
